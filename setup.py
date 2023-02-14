b55a92ee58f6337ade8f100a7cb2af6e49df21be
Vitalii <witalic2000@gmail.com>
2023-02-14 21:35:55 +0200

    add port 33507

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ app = Flask(__name__)
 def index():
     return  render_template("index.html")
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT'))
+app.run(host='0.0.0.0', port=33507)
